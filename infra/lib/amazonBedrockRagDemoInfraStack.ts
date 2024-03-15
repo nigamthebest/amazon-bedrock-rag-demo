74906b077a26f88ec943465bd56b79ca11a02cbd
Ankit Nigam <ankitni@amazon.com>
2024-03-15 16:59:06 +0200

    Init commit for app and docker image and infra

---
+++ infra/lib/amazonBedrockRagDemoInfraStack.ts
@@ -0,0 +1,43 @@
+
+
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from "constructs";
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
+
+export class amazonBedrockRagDemoInfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create an Amazon ECR repository if it doesn't exist
+    const ecrRepo = new ecr.Repository(this, 'MyEcrRepository', {
+      repositoryName: 'my-ecr-repo' // Change this to your desired repository name
+    });
+
+    // Create a Fargate cluster
+    const cluster = new ecs.Cluster(this, 'MyCluster', {
+      clusterName: 'my-fargate-cluster' // Change this to your desired cluster name
+    });
+
+    // Create a load-balanced Fargate service
+    const loadBalancedFargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'MyFargateService', {
+      cluster,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromEcrRepository(ecrRepo),
+        containerPort: 80 // Change this to your desired container port
+      },
+      desiredCount: 2, // Change this to your desired number of tasks
+      memoryLimitMiB: 512, // Change this to your desired memory limit
+      cpu: 256 // Change this to your desired CPU units
+    });
+
+    // Output the load balancer DNS name
+    new cdk.CfnOutput(this, 'LoadBalancerDNS', {
+      value: loadBalancedFargateService.loadBalancer.loadBalancerDnsName
+    });
+  }
+}
+
+const app = new cdk.App();
+new amazonBedrockRagDemoInfraStack(app, 'amazonBedrockRagDemoInfraStack');
